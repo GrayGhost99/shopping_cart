c1c8213b59314b3ca9b1e3e1b1e1321762b408b6
OL-Moore <micmoo9561@students.ecpi.edu>
2021-08-16 23:33:22 -0400

    Updates

---
+++ precache-manifest.49e75ccb8caf6d34128c47e820aef46a.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "d78e27906d034c3b4cd53aa3526acda3",
+    "url": "/shopping_cart/static/media/18532669286405344_1.d78e2790.jpg"
+  },
+  {
+    "revision": "d5702168d26f22240fac",
+    "url": "/shopping_cart/static/css/main.d55a502d.chunk.css"
+  },
+  {
+    "revision": "3a24ec788c8bd9527402",
+    "url": "/shopping_cart/static/js/1.3a24ec78.chunk.js"
+  },
+  {
+    "revision": "6ae39419816aacbf53a1",
+    "url": "/shopping_cart/static/js/runtime~main.6ae39419.js"
+  },
+  {
+    "revision": "b5714b6acfbff7904a24ea78662b9345",
+    "url": "/shopping_cart/static/media/100_1.b5714b6a.jpg"
+  },
+  {
+    "revision": "b618b5fc19f0517b8428bc9cae806df0",
+    "url": "/shopping_cart/static/media/101_1.b618b5fc.jpg"
+  },
+  {
+    "revision": "2e62289b2a88630ff3461c81af1756f7",
+    "url": "/shopping_cart/static/media/10412368723880252_1.2e62289b.jpg"
+  },
+  {
+    "revision": "54fbf816a3acf53727a2536d97d76881",
+    "url": "/shopping_cart/static/media/10547961582846888_1.54fbf816.jpg"
+  },
+  {
+    "revision": "0d941b4cc1da99273f35b2777b500ff3",
+    "url": "/shopping_cart/static/media/10686354557628304_1.0d941b4c.jpg"
+  },
+  {
+    "revision": "84c82a5a8f51a94e64024f67e8445d37",
+    "url": "/shopping_cart/static/media/11033926921508488_1.84c82a5a.jpg"
+  },
+  {
+    "revision": "06ec2c628b4a51a539881f0ee02b4e60",
+    "url": "/shopping_cart/static/media/11600983276356164_1.06ec2c62.jpg"
+  },
+  {
+    "revision": "7faf6a31b8557166976235d3bd9a8e2b",
+    "url": "/shopping_cart/static/media/11854078013954528_1.7faf6a31.jpg"
+  },
+  {
+    "revision": "2995d79a12f2b95cd2315d66c5bb5084",
+    "url": "/shopping_cart/static/media/12064273040195392_1.2995d79a.jpg"
+  },
+  {
+    "revision": "d5702168d26f22240fac",
+    "url": "/shopping_cart/static/js/main.d5702168.chunk.js"
+  },
+  {
+    "revision": "75f51cecfca671162c53bdeed3cf549f",
+    "url": "/shopping_cart/static/media/18644119330491310_1.75f51cec.jpg"
+  },
+  {
+    "revision": "1fc9c6c1b892f0cf4d7ead84937f388a",
+    "url": "/shopping_cart/static/media/2465747_1.1fc9c6c1.jpg"
+  },
+  {
+    "revision": "4c799bac69deb57ceba00bcdf15e6ec2",
+    "url": "/shopping_cart/static/media/27250082398145996_1.4c799bac.jpg"
+  },
+  {
+    "revision": "a313534ce6b430f29143cafee1eb5a3e",
+    "url": "/shopping_cart/static/media/39876704341265610_1.a313534c.jpg"
+  },
+  {
+    "revision": "df947f142806310944c26d76bf1b8fa0",
+    "url": "/shopping_cart/static/media/51498472915966370_1.df947f14.jpg"
+  },
+  {
+    "revision": "843a471381aada57cdd38f66662ab1af",
+    "url": "/shopping_cart/static/media/5619496040738316_1.843a4713.jpg"
+  },
+  {
+    "revision": "45a051e062934c657ca67819884cbf23",
+    "url": "/shopping_cart/static/media/6090484789343891_1.45a051e0.jpg"
+  },
+  {
+    "revision": "87b5353e208124cb9acc7cadd973a3bb",
+    "url": "/shopping_cart/static/media/8552515751438644_1.87b5353e.jpg"
+  },
+  {
+    "revision": "df97d7c2d0fc5e5d9346b660ce3936fa",
+    "url": "/shopping_cart/static/media/876661122392077_1.df97d7c2.jpg"
+  },
+  {
+    "revision": "59b643658d74a090ed326573a46e07a7",
+    "url": "/shopping_cart/static/media/9197907543445676_1.59b64365.jpg"
+  },
+  {
+    "revision": "9b698d6dbfaf10d84d983440e2b5a433",
+    "url": "/shopping_cart/static/media/100_2.9b698d6d.jpg"
+  },
+  {
+    "revision": "5aa0fc23f9605944f4c7a4d86f4268b2",
+    "url": "/shopping_cart/static/media/2465747_2.5aa0fc23.jpg"
+  },
+  {
+    "revision": "33aede85313995ed54e09dbb772b05f6",
+    "url": "/shopping_cart/index.html"
+  }
+];
